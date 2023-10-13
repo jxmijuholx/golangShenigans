ae05f7932450c6b939867acb500e38d302a0aacd
Jami Juhola <jami.juhola01@gmail.com>
2023-10-13 22:17:53 +0300

    printnbr

--- printnbr.go
+++ printnbr.go
@@ -3,7 +3,33 @@ package main
 import "github.com/01-edu/z01"
 
 func PrintNbr(n int) {
+	if n < 0 {
+		z01.PrintRune('-')
+		n = -n
+	} else if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
 
+	numCount := 0
+	nbr := n
+	for nbr > 0 {
+		nbr /= 10
+		numCount++
+	}
+
+	digits := make([]int, numCount)
+	i := 0
+	for n > 0 {
+		digit := n % 10
+		digits[i] = digit
+		n /= 10
+		i++
+	}
+
+	for j := i - 1; j >= 0; j-- {
+		z01.PrintRune(rune(digits[j]) + '0')
+	}
 }
 
 func main() {
